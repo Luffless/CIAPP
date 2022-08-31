5bc2edd2e3dd5ad5c1203f20b1532fcb542e7e9f
Eduardo <eduardo.lunelli@2020spaces.com>
2022-08-31 18:54:18 -0300

    Add project files.

---
+++ CIAPP/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIAPP
+{
+    public partial class LoginScreen : Form
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
