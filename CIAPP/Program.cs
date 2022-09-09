c04857b6bd3664e5c349240a771291800383ed23
Eduardo <eduardo.lunelli@2020spaces.com>
2022-09-09 19:36:33 -0300

    comit

--- CIAPP/Program.cs
+++ CIAPP/Program.cs
@@ -11,11 +11,12 @@ namespace CIAPP
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        /// ///tessste
+        /// ///tessstesss
         [STAThread]
         static void Main()
-        {
-            Application.EnableVisualStyles();
+        {   
+            ///aaaa
+            //Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
