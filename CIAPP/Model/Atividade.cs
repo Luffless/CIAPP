840ca33bc03daf74d09e76ca0c672ada5accb290
Eduardo <eduardo.lunelli@2020spaces.com>
2022-09-18 12:15:26 -0300

    Criado alguns models, verificar campos datas

---
+++ CIAPP/Model/Atividade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIAPP.Model
+{
+    public class Atividade
+    {
+        public int codigo { get; set; }
+        public string descricao { get; set; }
+    }
+}
---
+++ CIAPP/Model/Crime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIAPP.Model
+{
+    public class Crime
+    {
+        public string codigoPenal { get; set; }
+        public string descricao { get; set; }
+    }
+}
