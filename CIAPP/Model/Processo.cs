88563e8a330a99952c8ebe5b9de1ce62d81a6a5f
DanielLazzari <dlazzari3@ucs.br>
2022-10-05 19:20:40 -0300

    Ajustes na classe Processo

--- CIAPP/Model/Processo.cs
+++ CIAPP/Model/Processo.cs
@@ -5,12 +5,12 @@ public class Processo
 {
     public int Id { get; set; }
     public string VaraOrigem { get; set; }
-    public bool AcordoPersecucaoPenal { get; set; }
+    public int NumeroArtigoPenal { get; set; }
     public string PenaOriginaria { get; set; }
     public int HorasCumprir { get; set; }
+    public bool AcordoPersecucaoPenal { get; set; }
     public DateTime DataInicio { get; set; }
     public DateTime DataTermino { get; set; }
     public Prestador Prestador { get; set; }
-    public List<Crime> CrimeList { get; set; }
     public List<ProcessoEntidade> ProcessoEntidadeList { get; set; }
 }
