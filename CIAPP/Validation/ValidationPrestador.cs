44c0552bea9a55e5d3aedb85c6a77e5ff5eb7bbd
Daniel Lazzari <daniel.lazzari@tramontina.com>
2022-10-06 16:09:59 -0300

    Adicionado validacoes de CNPJ e CPF

--- CIAPP/Validation/ValidationPrestador.cs
+++ CIAPP/Validation/ValidationPrestador.cs
@@ -23,6 +23,80 @@ public class ValidationPrestador
             return false;
         }
 
+        string valor = cpf.Replace(".", "");
+        valor = valor.Replace("-", "");
+
+        bool igual = true;
+
+        for (int i = 1; i < 11 && igual; i++)
+        {
+            if (valor[i] != valor[0])
+            {
+                igual = false;
+            } 
+        }
+
+        if (igual || valor == "12345678909")
+        {
+            MessageBox.Show("CPF inválido!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            return false;
+        }
+
+        int[] numeros = new int[11];
+
+        for (int i = 0; i < 11; i++)
+        {
+            numeros[i] = int.Parse(valor[i].ToString());
+        }
+
+        int soma = 0;
+
+        for (int i = 0; i < 9; i++)
+        {
+            soma += (10 - i) * numeros[i];
+        }
+
+        int resultado = soma % 11;
+
+        if (resultado == 1 || resultado == 0)
+        {
+            if (numeros[9] != 0)
+            {
+                MessageBox.Show("CPF inválido!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+        }
+
+        if (numeros[9] != 11 - resultado)
+        {
+            MessageBox.Show("CPF inválido!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            return false;
+        }
+
+        soma = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            soma += (11 - i) * numeros[i];
+        }
+
+        resultado = soma % 11;
+
+        if (resultado == 1 || resultado == 0)
+        {
+            if (numeros[10] != 0)
+            {
+                MessageBox.Show("CPF inválido!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+        }
+
+        if (numeros[10] != 11 - resultado)
+        {
+            MessageBox.Show("CPF inválido!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            return false;
+        }
+
         if (prestadorDAO.ExisteCpf(id, cpf))
         {
             MessageBox.Show("CPF informado já existe!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
